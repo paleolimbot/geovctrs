9c701564f546ecf50a81490d8c37e272caf45460
Dewey Dunnington <dewey@fishandwhistle.net>
2020-03-30 12:01:54 -0300

    remove most geo_coord_ functions

--- R/geo-coord.R
+++ R/geo-coord.R
@@ -42,24 +42,7 @@ vec_ptype2.geo_coord.default <- function(x, y, ..., x_arg = "x", y_arg = "y") {
 #' @method vec_ptype2.geo_coord geo_coord
 #' @export
 vec_ptype2.geo_coord.geo_coord <- function(x, y, ..., x_arg = "x", y_arg = "y") {
-  # always pick 'multi' for common types
-  # slightly easier to do it here rather than using a million double dispatch
-  # methods
-  if (inherits(x, "geo_coord_point") && inherits(y, "geo_coord_multipoint")) {
-    new_geo_coord_multipoint()
-  } else if(inherits(x, "geo_coord_multipoint") && inherits(y, "geo_coord_point")) {
-    new_geo_coord_multipoint()
-  } else if (inherits(x, "geo_coord_linestring") && inherits(y, "geo_coord_multilinestring")) {
-    new_geo_coord_multilinestring()
-  } else if(inherits(x, "geo_coord_multilinestring") && inherits(y, "geo_coord_linestring")) {
-    new_geo_coord_multilinestring()
-  } else if (inherits(x, "geo_coord_polygon") && inherits(y, "geo_coord_multipolygon")) {
-    new_geo_coord_multipolygon()
-  } else if(inherits(x, "geo_coord_multipolygon") && inherits(y, "geo_coord_polygon")) {
-    new_geo_coord_multipolygon()
-  } else {
-    NextMethod()
-  }
+  vec_ptype(x)
 }
 
 #' @method vec_ptype2.geo_coord data.frame
@@ -125,23 +108,10 @@ vec_cast.geo_coord.default <- function(x, to, ...) {
 #' @method vec_cast.geo_coord geo_coord
 #' @export
 vec_cast.geo_coord.geo_coord <- function(x, to, ...) {
-  # always pick 'multi' for common types
-  # slightly easier to do it here rather than using a million double dispatch
-  # methods
-  if (inherits(x, "geo_coord_point") && inherits(to, "geo_coord_multipoint")) {
-    new_geo_coord_multipoint(vec_data(x))
-  } else if (inherits(x, "geo_coord_linestring") && inherits(to, "geo_coord_multilinestring")) {
-    data <- vec_data(x)
-    data$part <- rep_len(1L, vec_size(x))
-    new_geo_coord_multilinestring(data)
-  } else if (inherits(x, "geo_coord_polygon") && inherits(to, "geo_coord_multipolygon")) {
-    data <- vec_data(x)
-    data$part <- rep_len(1L, vec_size(x))
-    new_geo_coord_multipolygon(data)
-  } else if(identical(class(x), class(to))) {
+  if(identical(class(x), class(to))) {
     x
   } else {
-    NextMethod()
+    vec_cast.geo_coord.default(x, to)
   }
 }
 
@@ -173,19 +143,4 @@ vec_cast.geo_coord.list <- function(x, to, ...) {
 #' @export
 vec_cast.list.geo_coord <- function(x, to, ...) {
   vec_data(x)
-}
-
-#' @export
-geo_restore.geo_coord <- function(to, x) {
-  # the C++ data structure  for these is list() of geo_coords
-  vec_c(!!!x)
-}
-
-#' @export
-geo_size.geo_coord <- function(x) {
-  if (vec_size(x) > 0) {
-    max(field(x, "feature"))
-  } else {
-    0
-  }
 }
