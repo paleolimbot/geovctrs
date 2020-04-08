33abad4cfb1f137c44e36e06ef15534e3c57f8bd
Dewey Dunnington <dewey@fishandwhistle.net>
2020-04-08 20:28:51 -0300

    operators to header-only

--- src/geos-operator.h
+++ src/geos-operator.h
@@ -15,12 +15,30 @@ public:
   GEOSContextHandle_t context;
 
   virtual SEXP operate() = 0;
-  virtual void init();
-  virtual size_t maxParameterLength();
-  virtual void finish();
-  virtual size_t size();
-  virtual void finishProvider();
-  virtual ~Operator();
+
+  virtual void init() {
+
+  }
+
+  virtual size_t maxParameterLength() {
+    return 1;
+  }
+
+  virtual void finish() {
+
+  }
+
+  virtual size_t size() {
+    return this->commonSize;
+  }
+
+  virtual void finishProvider() {
+
+  }
+
+  virtual ~Operator() {
+
+  }
 };
 
 // ------------- unary operators ----------------
@@ -30,14 +48,82 @@ public:
   std::unique_ptr<GeometryProvider> provider;
   std::unique_ptr<GeometryExporter> exporter;
 
-  virtual void initProvider(SEXP provider, SEXP exporter);
-  virtual SEXP operate();
+  virtual void initProvider(SEXP provider, SEXP exporter) {
+    this->provider = resolve_provider(provider);
+    this->exporter = resolve_exporter(exporter);
+  }
+
+  virtual SEXP operate() {
+    this->initBase();
+    this->init();
+
+    // TODO: there is probably a memory leak here, but
+    // GEOSGeom_destroy_r(this->context, geometry) gives
+    // an error
+    GEOSGeometry* geometry;
+    GEOSGeometry* result;
+
+    try {
+      for (size_t i=0; i < this->size(); i++) {
+        checkUserInterrupt();
+        this->counter = i;
+        geometry = this->provider->getNext();
+
+        if (geometry == NULL) {
+          result = this->operateNextNULL();
+        } else {
+          result = this->operateNext(geometry);
+        }
+
+        this->exporter->putNext(result);
+      }
+    } catch(Rcpp::exception e) {
+      this->finish();
+      throw e;
+    }
+
+    this->finish();
+    return this->finishBase();
+  }
+
   virtual GEOSGeometry* operateNext(GEOSGeometry* geometry) = 0;
-  virtual GEOSGeometry* operateNextNULL();
+
+  virtual GEOSGeometry* operateNextNULL() {
+    return NULL;
+  }
 
 private:
-  void initBase();
-  SEXP finishBase();
+  void initBase() {
+    this->context = geos_init();
+    this->provider->init(this->context);
+
+    IntegerVector allSizes = IntegerVector::create(
+      this->maxParameterLength(),
+      this->provider->size()
+    );
+
+    IntegerVector nonConstantSizes = allSizes[allSizes != 1];
+    if (nonConstantSizes.size() == 0) {
+      this->commonSize = 1;
+    } else {
+      this->commonSize = nonConstantSizes[0];
+    }
+
+    for (size_t i=0; i<nonConstantSizes.size(); i++) {
+      if (nonConstantSizes[i] != this->commonSize) {
+        stop("Providers with incompatible lengths passed to BinaryGeometryOperator");
+      }
+    }
+
+    this->exporter->init(this->context, this->size());
+  }
+
+  SEXP finishBase() {
+    this->provider->finish();
+    SEXP value = this->exporter->finish();
+    geos_finish(this->context);
+    return value;
+  }
 };
 
 // ----- unary vector operators -----
@@ -46,13 +132,72 @@ class UnaryOperator: public Operator {
 public:
   std::unique_ptr<GeometryProvider> provider;
 
-  virtual void initProvider(SEXP provider);
-  virtual SEXP operate();
-  virtual void operateNext(GEOSGeometry* geometry) = 0;
-  virtual SEXP assemble();
+  virtual void initProvider(SEXP provider) {
+    this->provider = resolve_provider(provider);
+  }
 
-  void initBase();
-  SEXP finishBase();
+  virtual SEXP operate() {
+    this->initBase();
+    this->init();
+
+    // TODO: there is probably a memory leak here, but
+    // GEOSGeom_destroy_r(this->context, geometry) gives
+    // an error
+    GEOSGeometry* geometry;
+
+    try {
+      for (size_t i=0; i < this->size(); i++) {
+        checkUserInterrupt();
+        this->counter = i;
+        geometry = this->provider->getNext();
+        this->operateNext(geometry);
+      }
+    } catch(Rcpp::exception e) {
+      this->finish();
+      throw e;
+    } catch(std::exception e) {
+      this->finish();
+      throw e;
+    }
+
+    this->finish();
+    return this->finishBase();
+  }
+
+  virtual void operateNext(GEOSGeometry* geometry) = 0;
+
+  virtual SEXP assemble() {
+    return R_NilValue;
+  }
+
+  void initBase() {
+    this->context = geos_init();
+    this->provider->init(this->context);
+
+    IntegerVector allSizes = IntegerVector::create(
+      this->maxParameterLength(),
+      this->provider->size()
+    );
+
+    IntegerVector nonConstantSizes = allSizes[allSizes != 1];
+    if (nonConstantSizes.size() == 0) {
+      this->commonSize = 1;
+    } else {
+      this->commonSize = nonConstantSizes[0];
+    }
+
+    for (size_t i=0; i<nonConstantSizes.size(); i++) {
+      if (nonConstantSizes[i] != this->commonSize) {
+        stop("Providers with incompatible lengths passed to BinaryGeometryOperator");
+      }
+    }
+  }
+
+  SEXP finishBase() {
+    this->provider->finish();
+    geos_finish(this->context);
+    return this->assemble();
+  }
 };
 
 template <class VectorType, class ScalarType>
