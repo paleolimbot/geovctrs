92ef8bf733f7ebac6594790fe38911e1f73bf259
Dewey Dunnington <dewey@fishandwhistle.net>
2020-04-08 21:42:14 -0300

    consolidate constant provider construction

--- src/geos-provider.cpp
+++ src/geos-provider.cpp
@@ -3,84 +3,43 @@
 using namespace Rcpp;
 
 std::unique_ptr<GeometryProvider> resolve_provider(SEXP data) {
+  std::unique_ptr<GeometryProvider> provider;
+
   if (Rf_inherits(data, "geovctrs_wkt")) {
-    CharacterVector dataChar = (CharacterVector) data;
-
-    if (dataChar.size() ==  1) {
-      return std::unique_ptr<GeometryProvider> { new ConstantGeometryProvider(new WKTGeometryProvider(dataChar)) };
-    } else {
-      return std::unique_ptr<GeometryProvider> { new WKTGeometryProvider(dataChar) };
-    }
-
+    provider = std::unique_ptr<GeometryProvider> { new WKTGeometryProvider(data) };
   } else if(Rf_inherits(data, "geovctrs_wkb")) {
-    List dataList = (List) data;
-
-    if (dataList.size() ==  1) {
-      return std::unique_ptr<GeometryProvider> { new ConstantGeometryProvider(new WKBGeometryProvider(dataList)) };
-    } else {
-      return std::unique_ptr<GeometryProvider> { new WKBGeometryProvider(dataList) };
-    }
-
+    provider = std::unique_ptr<GeometryProvider> { new WKBGeometryProvider(data) };
   } else if(Rf_inherits(data, "geovctrs_xy")) {
-    List xy = (List)data;
-    if (Rf_length(xy[0]) ==  1) {
-      return std::unique_ptr<GeometryProvider> { new ConstantGeometryProvider(new XYProvider(data)) };
-    } else {
-      return std::unique_ptr<GeometryProvider> { new XYProvider(data) };
-    }
+    provider = std::unique_ptr<GeometryProvider> { new XYProvider(data) };
   } else if(Rf_inherits(data, "geovctrs_segment")) {
-    List segment = (List) data;
-
-    if (Rf_length(segment[0]) == 1) {
-      return std::unique_ptr<GeometryProvider> {
-        new ConstantGeometryProvider(new SegmentProvider(segment))
-      };
-    } else {
-      return std::unique_ptr<GeometryProvider> {
-        new SegmentProvider(segment)
-      };
-    }
+    provider = std::unique_ptr<GeometryProvider> { new SegmentProvider(data) };
   } else if(Rf_inherits(data, "geovctrs_rect")) {
-    List rect = (List) data;
-
-    if (Rf_length(rect[0]) ==  1) {
-      return std::unique_ptr<GeometryProvider> {
-        new ConstantGeometryProvider(new GeoRectProvider(rect))
-      };
-    } else {
-      return std::unique_ptr<GeometryProvider> {
-        new GeoRectProvider(rect)
-      };
-    }
+    provider = std::unique_ptr<GeometryProvider> { new GeoRectProvider(data) };
   } else if(Rf_inherits(data, "geovctrs_collection")) {
-    List col = (List) data;
-
-    if (Rf_length(col[0]) ==  1) {
-      return std::unique_ptr<GeometryProvider> { new ConstantGeometryProvider(new GeoCollectionProvider(col)) };
-    } else {
-      return std::unique_ptr<GeometryProvider> { new GeoCollectionProvider(col) };
-    }
+    provider = std::unique_ptr<GeometryProvider> { new GeoCollectionProvider(data) };
+  } else {
+    stop("Can't resolve GeometryProvider");
   }
 
-  stop("Can't resolve GeometryProvider");
+  if (provider->size() == 1) {
+    return std::unique_ptr<GeometryProvider> { new ConstantGeometryProvider(provider.release()) };
+  } else {
+    return provider;
+  }
 }
 
 std::unique_ptr<GeometryExporter> resolve_exporter(SEXP ptype) {
   if (Rf_inherits(ptype, "geovctrs_wkt")) {
-    return std::unique_ptr<GeometryExporter> {
-      new WKTGeometryExporter(ptype)
-    };
+    return std::unique_ptr<GeometryExporter> { new WKTGeometryExporter(ptype) };
   } else if(Rf_inherits(ptype, "geovctrs_wkb")) {
-    return std::unique_ptr<GeometryExporter> {
-      new WKBGeometryExporter(ptype)
-    };
+    return std::unique_ptr<GeometryExporter> { new WKBGeometryExporter(ptype) };
   } else if(Rf_inherits(ptype, "geovctrs_collection")) {
     return std::unique_ptr<GeometryExporter> { new GeoCollectionExporter() };
   } else if(Rf_inherits(ptype, "geovctrs_xy")) {
     return std::unique_ptr<GeometryExporter> { new XYExporter() };
   } else if(Rf_inherits(ptype, "geovctrs_segment")) {
     return std::unique_ptr<GeometryExporter> { new SegmentExporter() };
+  } else {
+    stop("Can't resolve GeometryExporter");
   }
-
-  stop("Can't resolve GeometryExporter");
 }
