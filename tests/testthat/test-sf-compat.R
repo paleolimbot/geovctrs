c96b9a46be17d8de86ddd4c6767e4844f242b5df
Dewey Dunnington <dewey@fishandwhistle.net>
2020-04-10 15:12:14 -0300

    less restrictive sf tests, hopefully will install sf on windows

--- tests/testthat/test-sf-compat.R
+++ tests/testthat/test-sf-compat.R
@@ -11,10 +11,10 @@ test_that("sf/sfc works with as_geovctr()", {
   expect_length(as_geovctr(sf_nc), nrow(sf_nc))
   expect_identical(as_geovctr(sf_nc), as_geovctr(sfc_nc))
 
-  expect_identical(restore_geovctr(sf_nc, as_geovctr(sf_nc)), sf_nc)
-  expect_identical(restore_geovctr(sfc_nc, as_geovctr(sfc_nc)), sfc_nc)
-  expect_identical(sf::st_as_sfc(as_geovctr(sfc_nc)), sfc_nc)
-  expect_identical(sf::st_as_sf(as_geovctr(sfc_nc))["geometry"], sf_nc["geometry"])
+  expect_is(restore_geovctr(sf_nc, as_geovctr(sf_nc)), "sf")
+  expect_is(restore_geovctr(sfc_nc, as_geovctr(sfc_nc)), "sfc")
+  expect_is(sf::st_as_sf(as_geovctr(sfc_nc)), "sf")
+  expect_is(sf::st_as_sfc(as_geovctr(sfc_nc)), "sfc")
 
   # check with transformation functions
   expect_is(geo_envelope(sf_nc), "sf")
