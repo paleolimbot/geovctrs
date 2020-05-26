19656d5635cb55a0c7af4c705aafcf812f2f5a45
Dewey Dunnington <dewey@fishandwhistle.net>
2020-05-26 13:33:30 -0300

    rename wk

--- R/wk.R
+++ R/wk.R
@@ -1,4 +1,86 @@
 
+#' @importFrom wk as_wkt
+#' @export
+wk::as_wkt
+
+#' @importFrom wk wkt
+#' @export
+wk::wkt
+
+#' @importFrom wk parse_wkt
+#' @export
+wk::parse_wkt
+
+#' @importFrom wk as_wkt
+#' @export
+as_wkt.geovctr <- function(x, ...) {
+  vec_cast(x, new_wk_wkt())
+}
+
+#' @importFrom wk validate_wk_wkt
+#' @importFrom wk vec_cast.wk_wkt
+#' @method vec_cast.wk_wkt geovctrs_rect
+#' @export
+vec_cast.wk_wkt.geovctrs_rect <- function(x, to, ...) {
+  geovctrs_cpp_convert(x, to)
+}
+
+#' @method vec_cast.wk_wkt geovctrs_segment
+#' @export
+vec_cast.wk_wkt.geovctrs_segment <- function(x, to, ...) {
+  geovctrs_cpp_convert(x, to)
+}
+
+#' @method vec_cast.wk_wkt geovctrs_xy
+#' @export
+vec_cast.wk_wkt.geovctrs_xy <- function(x, to, ...) {
+  geovctrs_cpp_convert(x, to)
+}
+
+#' @method vec_cast.wk_wkt geovctrs_xyz
+#' @export
+vec_cast.wk_wkt.geovctrs_xyz <- function(x, to, ...) {
+  geovctrs_cpp_convert(x, to)
+}
+
+#' @method vec_cast.wk_wkt geovctrs_collection
+#' @export
+vec_cast.wk_wkt.geovctrs_collection <- function(x, to, ...) {
+  geovctrs_cpp_convert(x, to)
+}
+
+#' @importFrom wk vec_ptype2.wk_wkt
+#' @method vec_ptype2.wk_wkt geovctrs_collection
+#' @export
+vec_ptype2.wk_wkt.geovctrs_collection <- function(x, y, ..., x_arg = "x", y_arg = "y") {
+  wkt()
+}
+
+#' @method vec_ptype2.wk_wkt geovctrs_xy
+#' @export
+vec_ptype2.wk_wkt.geovctrs_xy <- function(x, y, ..., x_arg = "x", y_arg = "y") {
+  wkt()
+}
+
+#' @method vec_ptype2.wk_wkt geovctrs_xyz
+#' @export
+vec_ptype2.wk_wkt.geovctrs_xyz <- function(x, y, ..., x_arg = "x", y_arg = "y") {
+  wkt()
+}
+
+#' @method vec_ptype2.wk_wkt geovctrs_segment
+#' @export
+vec_ptype2.wk_wkt.geovctrs_segment <- function(x, y, ..., x_arg = "x", y_arg = "y") {
+  wkt()
+}
+
+#' @method vec_ptype2.wk_wkt geovctrs_rect
+#' @export
+vec_ptype2.wk_wkt.geovctrs_rect <- function(x, y, ..., x_arg = "x", y_arg = "y") {
+  wkt()
+}
+
+
 #' @importFrom wk as_wkb
 #' @export
 wk::as_wkb
--- tests/testthat/test-wk.R
+++ tests/testthat/test-wk.R
@@ -50,3 +50,53 @@ test_that("coersion and casting works for wkt types", {
     as_wkt(geo_point(geo_xyz(1, 2, 3)), wkt())
   )
 })
+
+
+test_that("wkb casting and coersion works", {
+  wkb_raw <- as.raw(
+    c(
+      0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x3e, 0x40, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x24, 0x40
+    )
+  )
+
+  wkb <- wkb(list(wkb_raw))
+  expect_identical(vec_cast(wkb, wkb()), wkb)
+
+  wkt <- vec_cast(wkb, wkt())
+  wkb_roundtrip <- vec_cast(wkt, wkb())
+  expect_identical(wkb, wkb_roundtrip)
+  expect_identical(as_wkt(wkb), wkt)
+  expect_identical(as_wkb(wkt), wkb)
+
+  expect_identical(
+    vec_cast(as_wkb(wkt("POINT (1 2)")), geo_xy()),
+    geo_xy(1, 2)
+  )
+
+  expect_identical(
+    vec_cast(as_wkb(wkt("POINT Z (1 2 3)")), geo_xy()),
+    geo_xyz(1, 2, 3)
+  )
+
+  expect_identical(
+    vec_cast(as_wkb(wkt("POINT Z (1 2 3)")), geo_xyz()),
+    geo_xyz(1, 2, 3)
+  )
+
+  expect_identical(
+    vec_cast(geo_segment(geo_xy(0, 1), geo_xy(10, 11)), wkb()),
+    as_wkb(geo_segment(geo_xy(0, 1), geo_xy(10, 11)), wkb())
+  )
+
+  expect_identical(
+    vec_cast(geo_rect(0, 1, 10, 11), wkb()),
+    as_wkb(geo_rect(0, 1, 10, 11), wkb())
+  )
+
+  expect_identical(
+    vec_cast(geo_point(geo_xy(1, 2)), wkb()),
+    as_wkb(geo_point(geo_xy(1, 2)), wkb())
+  )
+})
